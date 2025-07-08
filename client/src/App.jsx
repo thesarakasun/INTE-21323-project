eed387a5ada8214675cc2ff7f694a3002f8d21e0
Thesara <thesarak03@gmail.com>
2025-07-08 11:28:11 +0530

    Frontend commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
