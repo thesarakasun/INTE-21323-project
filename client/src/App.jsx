8f23448a9ac52bf4f2f4b9c043a2d7b2f625575e
Thesara <thesarak03@gmail.com>
2025-07-23 20:38:57 +0530

    Main app updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import EmailVerified from './pages/EmailVerified';
+import RequestPasswordReset from './pages/RequestPasswordReset';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
- 
-
   return (
-    <div>
-      <BrowserRouter>
+    <>
+      <Navbar />
+      <div className="container mt-3">
         <Routes>
-          
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/verify-email/:token" element={<EmailVerified />} />
+          <Route path="/request-password-reset" element={<RequestPasswordReset />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
         </Routes>
-      </BrowserRouter>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
