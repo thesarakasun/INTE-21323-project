848c374d7d78228b4380bbfca1765795360dc99e
Praneesha Mendis <pnethpriya2002@gmail.com>
2025-07-23 20:55:15 +0530

    Utils added

---
+++ server/utils/validation.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const registerSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const emailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+const passwordResetSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = { registerSchema, loginSchema, emailSchema, passwordResetSchema };
