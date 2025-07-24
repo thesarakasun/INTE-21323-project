6ff4ee16ba55540efa0a530b5874fcf12c30ba03
Thesara <thesarak03@gmail.com>
2025-07-24 23:09:00 +0530

    Update server files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-app.listen(3001, () => {
-    console.log("Server is Running")
-})
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/auth', authRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
